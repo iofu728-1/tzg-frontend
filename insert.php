06bf6578744df3da92b80e0dba97034fa182d298
springxzy <441415133@qq.com>
2018-05-18 21:56:46 +0800

    html+php changed

--- insert.php
+++ insert.php
@@ -1,11 +1,15 @@
 <?php
 
    $name = $_POST['name'];
+   $sex = $_POST['sex'];
    $workCompany = $_POST['workCompany'];
+   $title = $_POST['title'];
    $startYear = $_POST['startYear'];
-   $educationLevel = $_POST['educationLevel'];
+   $graYear = $_POST['graYear'];
    $phoneNum = $_POST['phoneNum'];
    $email = $_POST['email'];
+   $party = $_POST['party'];
+   $hotel = $_POST['hotel'];
 
    $con = mysqli_connect("localhost","root","root");
 
@@ -15,8 +19,8 @@ if (!$con)
   }
 
 mysqli_select_db($con,"schoolmates");
-mysqli_query($con,"INSERT INTO forms (name, work_company, enter_year, education_level, phone_num, email)
-VALUES ('$name','$workCompany','$startYear','$educationLevel','$phoneNum','$email')");
+mysqli_query($con,"INSERT INTO forms (name,sex,work_company,title,enter_year,gra_year,phone_num,email,party,hotel)
+VALUES ('$name','$sex','$workCompany','$title','$startYear','$graYear','$phoneNum','$email','$party','$hotel')");
 
 mysqli_close($con);
 ?>
